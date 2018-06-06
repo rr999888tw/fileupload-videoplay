403eab0613e8ae4bcd6cdc206a27e45e26234ffe
rr999888tw <rr999888tw@gmail.com>
2018-06-06 16:51:22 +0800

    yo

---
+++ frontend/src/App.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ReactPlayer from 'react-player';
+// import { KSYVideo } from 'react-native-ksyvideo';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+// paddingTop: 32,
+import Typography from '@material-ui/core/Typography';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+
+
+
+const styles = {
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  },
+};
+
+
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // socket: null,
+      value: '',
+      files: null
+    };
+  }
+
+  
+
+  handleTextChange = (event) => {
+    this.setState({ value: event.target.value });
+  }
+
+  // handleTextSubmit = (event) => {
+
+  //   const data = {
+  //     url: this.state.value
+  //   }
+  //   const url = '/uploadText'
+  //   let options = {
+  //     method: "POST",
+  //     body: JSON.stringify(data),
+  //     headers: {
+  //       "Content-Type": "application/json"
+  //     },
+  //   };
+  //   fetch(url, options)
+  //     .then((response) => response.json())
+  //     .then((resjson) => { console.log(resjson); })
+  //     .catch((err) => { console.error(err) });
+
+
+  //   event.preventDefault();
+
+  //   this.setState({
+  //     value: ""
+  //   })
+
+  // }
+
+  handleFilesUpload = (filearr) => {
+    var formData = new FormData();
+
+    for (let i = 0; i < filearr.length; ++i) {
+      console.log({
+        fileName: filearr[i].name
+      })
+      formData.append(filearr[i].name, filearr[i]);
+    }
+
+    fetch('/uploadFiles', {
+      method: 'PUT',
+      body: formData
+    })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => { console.error('Error:', error) });
+  }
+
+  handleFilesChange = (e) => {
+    // console.log({ eTarget: e.target.value})
+    this.setState({ files: e.target.files })
+  }
+
+
+  handleFilesSubmit = (e) => {
+    e.preventDefault(); // Stop form submit
+    this.handleFilesUpload(this.state.files);
+    this.setState({
+      files: null
+    });
+    e.target[0].value = "";
+
+  }
+
+  render() {
+    const { classes } = this.props;
+    const myplaceholder = "...type in the file you want to play...";
+
+    return (
+      <div>
+
+        <form className={classes.root}
+          onSubmit={(e) => { this.handleFilesSubmit(e) }}>
+          <Input type="file" className={classes.root}
+            onChange={(e) => { this.handleFilesChange(e) }}
+            inputProps={{
+              multiple: true,
+            }} />
+          <Button type="submit" className={classes.root} variant='raised'> Upload </Button>
+        </form>
+
+
+        <form className={classes.root} onSubmit={(event) => { this.handleTextSubmit(event) }}>
+          <Input className={classes.root}
+            fullWidth={true}
+            placeholder={myplaceholder}
+            onChange={(event) => { this.handleTextChange(event) }}
+            value={this.state.value} />
+        </form>
+
+
+        <ReactPlayer 
+          url= {`/video/${this.state.value}`}  
+          playing = {false}
+          controls = {true}
+        />
+
+
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
---
+++ server/app.js
@@ -0,0 +1,91 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+import favicon from 'serve-favicon';
+import helmet from 'helmet';
+import compression from 'compression';
+import path from 'path';
+
+import env from './config/env';
+import routes from './routes';
+import fileUpload from 'express-fileupload';
+
+// import ejs  from 'ejs';
+// import stream  from 'express-stream';
+// import superagent  from 'superagent';
+
+import videoStream from 'video-stream';
+
+
+
+const app = express();
+app.use(fileUpload());
+
+/*==================================
+=            Middleware            =
+==================================*/
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use(helmet()); // Helmet helps you secure your Express apps by setting various HTTP headers.
+app.use(compression());
+app.use(logger('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// serve static files, this is for frontend React
+app.use('/static', express.static(path.join(__dirname, 'public', 'static')));
+
+/*=====  End of Middleware  ======*/
+
+/*===========================================
+=            Baic Authentication            =
+===========================================*/
+
+// app.use(require('node-basicauth')({'haochuan': 'password'}));
+
+/*=====  End of Baic Authentication  ======*/
+
+/*===========================
+=            COR            =
+===========================*/
+
+// app.use(require('cors')());
+
+/*=====  End of COR  ======*/
+
+// Routes
+app.use('/api/v1', routes.api_v1);
+app.use('/page', routes.page);
+
+
+// video-stream route
+app.get('/video/:filename', videoStream({ dir: path.resolve(__dirname, 'uploadedfiles') }));
+app.put('/uploadFiles', function (req, res) {
+  // console.log(req.files);
+  if (!req.files)
+    return res.status(400).send('No files were uploaded.');
+
+  var obj = req.files;
+  var arr = Object.keys(obj).map((key) => obj[key]);
+
+  for (let i = 0; i < arr.length; ++i) {
+    let file = arr[i]
+    file.mv(path.join(__dirname, 'uploadedfiles', file.name), function (err) {
+      if (err)
+        return res.status(500).send(err);
+
+      console.log(file.name + ' File uploaded!');
+    });
+  }
+
+  res.send('ok');
+});
+
+
+// Load React App
+// Serve HTML file for production
+if (env.name === 'production') {
+  app.get('*', function response(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+}
+
+export default app;
